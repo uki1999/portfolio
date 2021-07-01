6c3d674f9e3804385485970f5e3d5a60a220e36a
uki1999 <urosradisavljevic10@gmail.com>
2021-07-02 00:30:57 +0200

    Poruka

--- contact-form-process.php
+++ contact-form-process.php
@@ -1,14 +1,13 @@
 <?php
-  $name = htmlspecialchars($_POST['name']);
-  $email = htmlspecialchars($_POST['email']);
-  $phone = htmlspecialchars($_POST['phone']);
-  $website = htmlspecialchars($_POST['website']);
-  $message = htmlspecialchars($_POST['message']);
+  $name = htmlspecialchars($_POST['Name']);
+  $email = htmlspecialchars($_POST['Email']);
+  $text = htmlspecialchars($_POST['Text']);
+  $message = htmlspecialchars($_POST['Message']);
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
       $receiver = "receiver_email_address"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
+      $body = "Name: $name\nEmail: $email\nPhone: $text\nProject: $website\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
          echo "Your message has been sent";
